c32d5a029b2aa8791d514d635f5de671bf2c4780
Will Dages <wdages@gmail.com>
2014-10-11 13:43:20 -0400

    Add code from screencast

---
+++ closest-beacon-demo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  closest-beacon-demo
+//
+//  Created by Will Dages on 10/11/14.
+//  @willdages on Twitter
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D"), identifier: "Estimotes")
+    let colors = [
+        54482: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        31351: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
+        27327: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor]
+        }
+    }
+    
+}
+
---
+++ closest-beacon-demoTests/closest_beacon_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  closest_beacon_demoTests.swift
+//  closest-beacon-demoTests
+//
+//  Created by Will Dages on 10/11/14.
+//  @willdages on Twitter
+//
+
+import UIKit
+import XCTest
+
+class closest_beacon_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

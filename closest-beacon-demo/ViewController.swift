d9422a7c17b59447f6fe691ea6a18fff27b2c7d0
Will Dages <wdages@gmail.com>
2016-09-14 10:39:50 -0400

    Update to Swift3 syntax

--- closest-beacon-demo/ViewController.swift
+++ closest-beacon-demo/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
-    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, identifier: "Estimotes")
+    let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, identifier: "Estimotes")
     // Note: make sure you replace the keys here with your own beacons' Minor Values
     let colors = [
         54482: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
@@ -24,10 +24,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.delegate = self
-        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse) {
             locationManager.requestWhenInUseAuthorization()
         }
-        locationManager.startRangingBeaconsInRegion(region)
+        locationManager.startRangingBeacons(in: region)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,11 +35,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
-        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.unknown }
         if (knownBeacons.count > 0) {
             let closestBeacon = knownBeacons[0] as CLBeacon
-            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+            self.view.backgroundColor = self.colors[closestBeacon.minor.intValue]
         }
     }
     
--- closest-beacon-demoTests/closest_beacon_demoTests.swift
+++ closest-beacon-demoTests/closest_beacon_demoTests.swift
@@ -28,7 +28,7 @@ class closest_beacon_demoTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
